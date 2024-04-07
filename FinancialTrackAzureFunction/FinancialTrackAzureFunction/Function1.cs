a2bb2110ccbe583884301ccca5a19896fad1932b
asquare777 <148975085+asquare777@users.noreply.github.com>
2024-04-08 01:52:20 +0500

    Update Function1.cs

--- FinancialTrackAzureFunction/FinancialTrackAzureFunction/Function1.cs
+++ FinancialTrackAzureFunction/FinancialTrackAzureFunction/Function1.cs
@@ -9,7 +9,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyNamespace
+namespace FinancialTrackAzureFunction
 {
     public static class Function1
     {
